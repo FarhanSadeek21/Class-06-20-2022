ab83b5777e79a89b094c9941174ea0011e8b6b80
FarhanSadeek21 <farhansadeek019@gmail.org>
2022-06-20 20:47:48 -0400

    fifth commit

--- billboard.R
+++ billboard.R
@@ -1,3 +1,14 @@
 library(billboard)
 
-View(billboard)
+x <- wiki_hot_100s |>
+    as_tibble()
+View(x)
+
+
+x <- x |>
+    select(artist) |>
+    group_by(artist) |>
+    summarize(artist_freq = n()) |>
+    arrange(desc(artist_freq)) |>
+    head(10)
+View(x)
